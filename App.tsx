d84c2cdc1de865748f59f34b5620668d76108c0b
Jaskaran singh <jaskaran.singh@almabay.com>
2024-04-25 09:53:00 +0530

    comment

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {View, Text, StatusBar, StyleSheet} from 'react-native';
+import { View, Text, StatusBar, StyleSheet, ScrollView } from 'react-native';
 
 const quotes = [
-  "The only way to do great work is to love what you do. - Steve Jobs", 
+  "The only way to do great work is to love what you do. - Steve Jobs",
   "Innovation distinguishes between a leader and a follower. - Steve Jobs",
   "Your time is limited, don't waste it living someone else's life. - Steve Jobs",
   "Stay hungry, stay foolish. - Steve Jobs",
@@ -13,11 +13,13 @@ const App = () => {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor="#ffffff" barStyle="dark-content" />
+      <Text style={styles.cont}>Cards</Text>
       <View style={styles.cardContainer}>
         {quotes.map((quote, index) => (
-          <View key={index} style={styles.card}>
+          <ScrollView key={index} style={styles.card}>
             <Text style={styles.quote}>{quote}</Text>
-          </View>
+
+          </ScrollView>
         ))}
       </View>
     </View>
@@ -35,27 +37,34 @@ const styles = StyleSheet.create({
   cardContainer: {
     flexDirection: 'row',
     justifyContent: 'space-evenly',
-    flexWrap:"wrap",
+    flexWrap: "wrap",
   },
   card: {
     width: 150,
-    height: 150,
-    margin:10,
+    height: "auto",
+    margin: 10,
     backgroundColor: '#ffffff',
     borderRadius: 10,
     elevation: 5, // For Android
     shadowColor: '#000000',
-    shadowOffset: {width: 0, height: 2},
+    shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 2,
     marginHorizontal: 5,
     padding: 10,
   },
   quote: {
-    fontSize: 17,
-    fontStyle: 'italic',
+    fontSize: 18,
+    fontStyle: "normal",
     textAlign: 'center',
+    color:"blue"
   },
+  cont:{
+  fontSize:40,
+  color:"red",
+  marginBottom:60
+
+  }
 });
 
 export default App;
